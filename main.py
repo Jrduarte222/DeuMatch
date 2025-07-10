09cdb04f6c82b89f7a86965d859825e1da0ce83c
Jrduarte222 <duartejucara@gmail.com>
2025-07-10 16:26:39 -0300

    correção movimento

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from routes import users, chat
+
 from database import Base, engine
-from routes import movimentos
+from routes import users, chat, movimento  # Importando tudo de uma vez (boa prática)
 
 app = FastAPI()
-app.include_router(movimentos.router)
 
 # Inclusão das rotas
 app.include_router(users.router)
 app.include_router(chat.router)
+app.include_router(movimento.router)
 
 # Middleware CORS
 app.add_middleware(
